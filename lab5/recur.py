1cd10c7855c2d17722011b4c125c72e90c16eacb
l3eamster <beambetam@gmail.com>
2019-10-22 13:17:18 +0700

    lab4

---
+++ lab5/recur.py
@@ -0,0 +1,110 @@
+def eat(n):     
+    if n==1:         
+        print('eat', n)     
+    else:    
+        eat(n-1)       
+        print('eat', n)   # line 1         
+                # line 2  
+        
+def fac(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fac(n-1)
+
+# print(fac(5))
+
+def sum1ToN(n):
+    if n < 1:
+        return 0
+    else:
+        return n + sum1ToN(n-1)
+
+def print1ToN (n):
+    if n == 1:
+        print(1, end=' ')
+    else:
+        print1ToN(n-1)
+        print(n, end=' ')
+        
+def fib(n): 
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return  fib(n-2) + fib(n-1)
+
+def binarySearch(lo, hi, x, l):
+    mid = (lo + hi)//2
+
+    if l[mid] == x:
+        return mid
+    elif lo == hi:
+        return None
+    else:
+        if x > l[mid]:
+            lo = mid + 1
+        elif x < l[mid]:
+            hi = mid - 1
+        return binarySearch(lo, hi, x, l)
+
+    
+def move(n,start, dest):
+    if n == 1:
+        print(n, 'move from',start, 'to', dest)
+    else:
+        l = ['A', 'B', 'C']
+        start = l.pop(l.index(start))
+        dest = l.pop(l.index(dest))
+        temp = l[0]
+        move(n-1,start, temp)
+        print(n, 'move from', start, 'to', dest)
+        move(n-1, temp, dest)
+
+def sum1(n, l):
+    if n == 0:
+        return 0
+    else:
+        return l[n-1] + sum1(n-1, l)
+
+def sum2():
+    return None
+
+
+
+l = [1,2,3,4,5,6,7,8]
+#print(binarySearch(0, 7, 1, l))
+#move(3,'A','C')
+print(sum1(3,l))
+
+# def printSack(sack, maxi):     
+#     global good     
+#     global name     
+#     for i in range (maxi+1):         
+#         print(name[sack[i]], good[sack[i]], end = ' ')         # print(name[sack[i]],good[sack[i]], end = ' ')     
+#     print() 
+ 
+# def pick(sack, i, mLeft, ig):     
+#     global N     
+#     global good     
+#     if ig < N:    # have something left to pick         
+#         price = good[ig]    # good-price         
+#         if mLeft < price:   # cannot afford that ig             
+#             pick(sack, i, mLeft, ig+1) # try to pick next good          
+#         else:               # can buy             
+#             mLeft -= price  # pay             
+#             sack[i] = ig    # pick that ig to the sack at i             
+#             if mLeft == 0:  # done                 
+#                 printSack(sack, i)             
+#             else:           # still have moneyLeft                 
+#                 pick(sack, i+1, mLeft, ig+1)             
+#             pick(sack, i, mLeft+price, ig+1)  # take the item off the sack for other solutions  
+ 
+# good = [20,10,5,5,3,2,20,10] 
+# name = ['soap', 'potato chips', 'loly pop', 'toffy', 'pencil', 'rubber', 'milk','cookie'] 
+# N = len(good)   # numbers of good 
+ 
+# sack = N*[-1]   # empty sack 
+# mLeft = 20      # money left 
+# i = 0           # sack index 
+# ig = 0          # good index 
+# pick(sack, i, mLeft,ig)  
--- lab7/tree.py
+++ lab7/tree.py
@@ -3,26 +3,98 @@ class node:
         self.data = data     
         self.left = None if left is None else left     
         self.right = None if right is None else right 
-def __str__(self):     
-    return str(self.data)  
- 
-def getData(self):         # accessor     
-    return self.data 
- 
-def getLeft(self):         # accessor     
-    return self.left 
- 
-def getRight(self):        # accessor     
-    return self.right 
- 
-def setData(self, data):   # mutator     
-    self.data = data 
- 
-def setLeft(self, left):   # mutator     
-    self.left = left 
- 
-def setRight(self, right): # mutator     
-    self.right = right 
 
-class BST:
-    def __init__(self, root)
+    def __str__(self):     
+        return str(self.data)  
+    
+    def getData(self):         # accessor     
+        return self.data 
+    
+    def getLeft(self):         # accessor     
+        return self.left 
+    
+    def getRight(self):        # accessor     
+        return self.right 
+    
+    def setData(self, data):   # mutator     
+        self.data = data 
+    
+    def setLeft(self, left):   # mutator     
+        self.left = left 
+    
+    def setRight(self, right): # mutator     
+        self.right = right 
+
+class BST:     
+    def __init__(self, root = None):         
+        self.root = None if root is None else root 
+    
+    def addI(self, data):         
+        if self.root is None:             
+            self.root = node(data)         
+        else:             
+            fp = None       #father of p             
+            p = self.root   #start comparing from root             
+            while p:    # while p is not None                 
+                fp = p                 
+                p = p.left if data < p.data else p.right    # if data < p.data            
+                                                            #    p = p.left              
+                                                            # else:                  
+                                                            #    p = p.right             
+            if data < fp.data:                 
+                fp.left = node(data)             
+            else:                 
+                fp.right = node(data) 
+ 
+    def add(self, data):         
+        self.root = BST._add(self.root, data) 
+ 
+    def _add(root, data):   # recursive _add         
+        if root is None:             
+            return node(data)         
+        else:             
+            if data < root.data:                 
+                root.left = BST._add(root.left, data)             
+            else:                 
+                root.right = BST._add(root.right, data)         
+            return root      
+            
+    def inOrder(self):         
+        BST._inOrder(self.root)         
+        print() 
+ 
+    def _inOrder(root):         
+        if root:   # if root is not None             
+            BST._inOrder(root.left)             
+            print(root.data, end = ' ')             
+            BST._inOrder(root.right) 
+ 
+    def printSideway(self):         
+        BST._printSideway(self.root, 0)         
+        print() 
+ 
+    def _printSideway(root, level):         
+        if root :  # if root is not None             
+            BST._printSideway(root.right, level+1)             
+            print('   '*level, root.data, sep = '' )       
+            BST._printSideway(root.left, level+1) 
+
+    # def preOrder(self): 
+    
+    # def postOrder(self):
+
+    # def search(self, data):  
+
+    # def path(self, data):
+
+    # def del(self, data):
+
+l = [int(e) for e in input("insert integers : ").split()] 
+print(l) 
+ 
+t = BST() 
+for ele in l:     
+    t.addI(ele) 
+ 
+t.inOrder() 
+t.printSideway()
