ab75408194a4552d9907cbe08e3c45e96d820147
l3eamster <beambetam@gmail.com>
2019-10-29 13:05:27 +0700

    lab8

--- lab8/tree2.py
+++ lab8/tree2.py
@@ -3,6 +3,9 @@ class node:
         self.data = data        
         self.left = left if left is not None else None        
         self.right = right if right is not None else None 
+
+    def __str__(self):
+        return str(self.data)
 #---------------------No class BST 
 
 def inOrder(r):     
@@ -11,7 +14,7 @@ def inOrder(r):
         print(r.data, end = ' ')         
         inOrder(r.right) 
  
-def addi(r, data):  #ต้องแก้นาจา   
+def addi(r, data):   
     if not r:         
         return node(data)     
     else:         
@@ -43,7 +46,7 @@ def height(r):
         return -1     
     else:         
         hl = height(r.left)         
-        hr = height(r.right)         
+        hr = height(r.right)        
         if hl>hr:             
             return hl+1         
         else:             
@@ -72,8 +75,31 @@ def search(r, d):
         else:             
             return search(r.right, d) 
 
-def depth(r, d)
-    return dept
+def getFather(r, data):
+    if r.data == data:
+        return str(None) + ' ' + str(data) + ' ' + 'is ROOT'
+    else:
+        p = r
+        while True:
+            fp = p
+            if data < p.data:
+                p = p.left
+            elif data > p.data:
+                p = p.right
+
+            if data == p.data:
+                break
+        return fp
+
+def depth(r, d):
+    node = search(r, d)
+    return height(node)
+
+def smallest(r):
+    if r.left == None:
+        return r
+    if r:
+        return smallest(r.left)
 
 
 l = [14,4,9,7,15,3,18,16,20,5,16] 
@@ -88,7 +114,7 @@ inOrder(r)
 print() 
  
 print('printSideWay:') 
-printSideWay(r, 0) 
+printSideWay(r, 0)
  
 print('height of ', r.data, '=',  height(r)); 
  
@@ -99,6 +125,18 @@ path(r, d)
 d = 9 
 t = search(r, d) 
 print(t.data) 
+
+dfather = 14
+print('father of', dfather, 'is', end=' ')
+print(getFather(r, dfather))
+#print(dfather, 'is ROOT')
  
 d = 18 
-#print('depth of node data ', d, '=', depth(r, d)) 
+print('depth of node data ', d, '=', depth(r, d)) 
+#print(depth(r, d))
+
+print('smallest data = ', end='')
+print(smallest(r))
+
+
+
