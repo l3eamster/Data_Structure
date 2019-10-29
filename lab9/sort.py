dbb0a5b1f3ecfb7ed677c3a74f203a61574b37ac
l3eamster <beambetam@gmail.com>
2019-10-29 16:38:34 +0700

    sort

--- lab9/sort.py
+++ lab9/sort.py
@@ -1,6 +1,7 @@
 from math import log2 
 from math import floor 
-print('------------------------- SORT -------------------------')
+print('')
+print('---------------------------- SORT ----------------------------')
 def bubble(l):
     for last in range(len(l)-1, -1, -1):
         swap = False
@@ -59,7 +60,96 @@ l = [10,11,1,13,2,6,4,12,5,8,7,9,3]
 dIncrements = [5,3,1] 
 print('shell sort     >>', shell(l, dIncrements))
 
+def merge(l, left, right, rightEnd): 
+    start = left 
+    leftEnd = right-1 
+    result = [] 
+    while left <= leftEnd and right <= rightEnd: 
+        if l[left] < l[right]: 
+            result.append(l[left]) 
+            left += 1 
+        else: 
+            result.append(l[right]) 
+            right += 1 
+    while left <= leftEnd: # copy remaining left half if any 
+        result.append(l[left]) 
+        left += 1 
+    while right <= rightEnd: # copy remaining right half if any 
+        result.append(l[right]) 
+        right += 1
 
+    for ele in result: # copy result back to list l 
+        l[start] = ele 
+        start += 1 
+        if start > rightEnd: 
+            break
+
+def mergeSort(l, left, right): 
+    center =  (left+right)//2 
+    if left < right: 
+        mergeSort(l, left, center) 
+        mergeSort(l, center+1, right) 
+        merge(l, left, center+1, right)
+    return l
+l = [5,3,6,1,2,7,8,4] 
+print('merge sort     >>', mergeSort(l,0, len(l)-1))
+
+# def quick(l, left, right): 
+#     if left == right+1: #only 2 elements 
+#         if l[left] > l[right]:
+#             l[left], l[right] = l[right], l[left] #swap 
+#         return 
+#     if left < right: 
+#         #partition 
+#         pivot = l[left] #first element pivot 
+#         i, j = left+1, right 
+#         while i<j: 
+#             while i<right and l[i]<=pivot: 
+#                 i += 1 
+#             while j>left and l[j]>=pivot: 
+#                 j -= 1 
+#             if i < j: 
+#                 l[i], l[j] = l[j], l[i] #swap 
+#         if left is not j: 
+#             l[left], l[j] = l[j], pivot # swap pivot to index j 
+#         quick(l, left, j-1) 
+#         quick(l, j+1, right)
+#     return l
+
+# l = [5,1,4,9,6,3,8,2,7,0] 
+# print('quick sort     >>', quick(l,0,len(l)-1))
+
+def quick(l, left, right): 
+    """last element pivot""" 
+    if left == right+1: #only 2 elements 
+        if l[left] > l[right]: 
+            l[left], l[right] = l[right], l[left] #swap 
+            return 
+    if left < right: 
+        #partition 
+        pivot = l[right] #last element pivot 
+        #print('-------------------------------------') 
+        #print(' 0  1  2  3  4  5  6  7  8  9 ', ' (left=', left, ' right=', right, ' pivot=', pivot,')', sep='') 
+        #print(l)        
+        i, j = left, right-1 
+        while i<j: 
+            while i<right and l[i]<=pivot: 
+                i += 1 
+            while j>left and l[j]>=pivot: 
+                j -= 1 
+            if i<j: 
+                l[i], l[j] = l[j], l[i] #swap 
+                #print(l , '(swap', l[i], '-',l[j],')') 
+        if right is not i: 
+            l[right], l[i] = l[i], pivot # swap pivot to index i 
+            #print(l, '(swap pivot',l[i] , '-',l[right],')') 
+        quick(l, left, i-1) 
+        quick(l, i+1, right) 
+    return l
+        
+        
+l = [5,1,4,9,0,3,8,2,7,6] 
+print('quick sort     >>', quick(l,0,len(l)-1))
 
 # def print90(h, i, max_i): 
 #     if i < max_i: 
@@ -69,4 +159,5 @@ print('shell sort     >>', shell(l, dIncrements))
 #         print90(h, (i*2)+1, max_i)
 
 
-print('--------------------------------------------------------')
+print('--------------------------------------------------------------')
+print('')
